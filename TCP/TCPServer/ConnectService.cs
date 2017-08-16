feff1c9b97f090e4e1fffcbea93b29be2188f54f
chengwengao <657325704@qq.com>
2017-08-16 18:40:48 +0800

    添加进制转化公共方法

--- TCP/TCPServer/ConnectService.cs
+++ TCP/TCPServer/ConnectService.cs
@@ -18,9 +18,8 @@ namespace TCPServer
 
         public void Process()
         {
-            byte[] buffer = new byte[512 * 1024];
-            string content;
-
+            byte[] buffer = new byte[1 * 1024];
+            string content = "";
             string ipAddr = ((IPEndPoint)clientSocket.RemoteEndPoint).Address.ToString();
             Console.WriteLine("a new connect from:{0}", ipAddr);
 
@@ -29,12 +28,19 @@ namespace TCPServer
                 int received = 0;
                 while ((received = clientSocket.Receive(buffer)) != 0)
                 {
-                    content = Encoding.UTF8.GetString(buffer, 0, received);
-                    Console.WriteLine("received:{0}", content);
 
-                    content = string.Format("server echo:{0}", content);
-                    buffer = Encoding.UTF8.GetBytes(content);
-                    clientSocket.Send(buffer);
+                    content += BitConverter.ToString(buffer).Replace("-", " ");
+
+                    //content = Encoding.UTF8.GetString(buffer, 0, received);
+
+                    //content = string.Format("server echo:{0}", content);
+                    //buffer = Encoding.UTF8.GetBytes(content);
+                    //clientSocket.Send(buffer);
+                    Console.WriteLine("received:{0}", content);
+                    byte[] bytes = ExplainUtils.HexSpaceStringToByteArray(content);
+                    string terminalPhone = (this.ParseBcdStringFromBytes(bytes, 4 + 1, 6));
+                    Console.WriteLine(terminalPhone);
+
                 }
             }
             catch (Exception e)
@@ -44,6 +50,40 @@ namespace TCPServer
 
             clientSocket.Close();
             Console.WriteLine("connect from {0} closed.", ipAddr);
+        }
+
+        private String ParseBcdStringFromBytes(byte[] data, int startIndex, int lenth)
+        {
+            return this.ParseBcdStringFromBytes(data, startIndex, lenth, null);
+        }
+
+        private String ParseBcdStringFromBytes(byte[] data, int startIndex, int lenth, String defaultVal)
+        {
+            try
+            {
+                byte[] tmp = new byte[lenth];
+                Buffer.BlockCopy(data, startIndex, tmp, 0, lenth);
+                return this.bcd2String(tmp);
+            }
+            catch (Exception e)
+            {
+                //log.Error(string.Format("解析BCD(8421码)出错:{0}", e.Message));
+                //e.printStackTrace();
+                return defaultVal;
+            }
+        }
+
+        public String bcd2String(byte[] bytes)
+        {
+            StringBuilder temp = new StringBuilder(bytes.Length * 2);
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                // 高四位
+                temp.Append((bytes[i] & 0xf0) >> 4);
+                // 低四位
+                temp.Append(bytes[i] & 0x0f);
+            }
+            return temp.ToString().Substring(0, 1) == ("0") ? temp.ToString().Substring(1) : temp.ToString();
         }
     }
 }
---
+++ TCP/TCPServer/ExplainUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCPServer
+{
+    /// <summary>
+    /// 报文解析工具类
+    /// </summary>
+    class ExplainUtils
+    {
+            /// <summary>
+            /// 带空格16进制字符串转换为字节数组
+            /// </summary>
+            /// <param name="hexSpaceString">带空格的十六进制字符串</param>
+            /// <returns></returns>
+            public static byte[] HexSpaceStringToByteArray(string hexSpaceString)
+            {
+                hexSpaceString = hexSpaceString.Replace(" ", string.Empty);
+                if (hexSpaceString.Length % 2 != 0)
+                {
+                    hexSpaceString += " ";
+                }
+                byte[] array = new byte[hexSpaceString.Length / 2];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = Convert.ToByte(hexSpaceString.Substring(i * 2, 2), 16);
+                }
+                return array;
+            }
+    }
+}
